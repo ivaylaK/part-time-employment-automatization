1577e899e41d5dbf8984a405c418992b15a6c62f
ivaylaK <144151547+ivaylaK@users.noreply.github.com>
2025-06-29 14:07:10 +0200

    Updated mappers backend

--- backend/src/main/java/com/trenkwalder/parttimeemployment/mapper/JobMapper.java
+++ backend/src/main/java/com/trenkwalder/parttimeemployment/mapper/JobMapper.java
@@ -2,8 +2,12 @@ package com.trenkwalder.parttimeemployment.mapper;
 
 import com.trenkwalder.parttimeemployment.dto.JobDto;
 import com.trenkwalder.parttimeemployment.entity.Job;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.spi.MappingContext;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
 
 @Component
 public class JobMapper implements Mapper<Job, JobDto> {
@@ -12,6 +16,32 @@ public class JobMapper implements Mapper<Job, JobDto> {
 
     public JobMapper(ModelMapper modelMapper) {
         this.modelMapper = modelMapper;
+
+        Converter<String, LocalTime> stringToLocalTime = new Converter<>() {
+
+            @Override
+            public LocalTime convert(MappingContext<String, LocalTime> context) {
+                return context.getSource() == null ? null : LocalTime.parse(context.getSource());
+            }
+        };
+
+        Converter<LocalTime, String> localTimeToString = new Converter<>() {
+
+            @Override
+            public String convert(MappingContext<LocalTime, String> context) {
+                return context.getSource() == null ? null : context.getSource().toString();
+            }
+        };
+
+        modelMapper.typeMap(JobDto.class, Job.class).addMappings(mapper -> {
+            mapper.using(stringToLocalTime).map(JobDto::getShiftStart, Job::setShiftStart);
+            mapper.using(stringToLocalTime).map(JobDto::getShiftEnd, Job::setShiftEnd);
+        });
+
+        modelMapper.typeMap(Job.class, JobDto.class).addMappings(mapper -> {
+            mapper.using(localTimeToString).map(Job::getShiftStart, JobDto::setShiftStart);
+            mapper.using(localTimeToString).map(Job::getShiftEnd, JobDto::setShiftEnd);
+        });
     }
 
     @Override
--- backend/src/main/java/com/trenkwalder/parttimeemployment/mapper/MapperConfig.java
+++ backend/src/main/java/com/trenkwalder/parttimeemployment/mapper/MapperConfig.java
@@ -1,4 +1,4 @@
-package com.trenkwalder.parttimeemployment.mapper.config;
+package com.trenkwalder.parttimeemployment.mapper;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
